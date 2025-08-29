a42b3a9e2210179926fb07c0a3bcf86ab1be6236
Gal Yedidia <yedidia.gal@gmail.com>
2025-08-29 19:42:11 +0000

    I do not want to upload the 16x16 or 32x32 - it is too small - I uploade

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,10 @@ import { AuthProvider } from '@/lib/auth';
 export const metadata: Metadata = {
   title: 'ShuttleTrack',
   description: 'Badminton Training Attendance Application',
+  icons: {
+    icon: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
+  }
 };
 
 export default function RootLayout({
@@ -18,9 +22,6 @@ export default function RootLayout({
   return (
     <html lang="he" dir="rtl">
       <head>
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
